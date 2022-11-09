2b56a9127c0f03dd536464fb48b2fd72d673d91f
Марк Неловка <marknelovka@MacBook-Air-Mark.local>
2022-11-09 23:50:26 +0200

    add start files

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
+    </div>
+  )
+}

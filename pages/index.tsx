96f6402c097e42b8081c948aec514ab28693d947
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2022-11-11 19:45:14 +0200

    added stars

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
+import Head from 'next/head';
 import Image from 'next/image';
-import { type } from 'os';
+import { MouseEventHandler, useEffect } from 'react';
 
 interface IDataItems {
   id: number,
@@ -24,34 +25,53 @@ interface IData {
 }
 
 export default function Home({ data }: IData) {
-//   new google.maps.Geocoder().geocode({
-//   'latLng': new google.maps.LatLng(12.9715987, 77.594562699)
-// }, function(results, status) {
-//   if (status == google.maps.GeocoderStatus.OK) {
-//     console.log(results);
-//   }
-// });
+  // console.log(data);
+
+  // useEffect(() => {
+  //  const res =
+  // }, [third])
+  
+  const getStars = (event: React.MouseEvent) => {
+    const { value } = event.target as HTMLInputElement;
+    const { id } = event.currentTarget as HTMLLIElement;
+    console.log(value);
+    console.log(id);
+  }
+
   return (
     <main>
       <section className='bg-[#E6E9F2] py-7 px-64'>
          <ul>
       {data.map(({id, pictures, title, name, address, location, benefits, phone}: IDataItems) => {
-        return <li key={id} className="bg-white mb-2 px-4 py-6 flex items-center">
+        return <li key={id} className="bg-white mb-2 px-4 py-6 flex items-center" onClick={getStars} id={String(id)}>
           <div className='mr-6'>
                <Image src={pictures[0]} alt="img" width={85} height={85} className="rounded-[50%] h-[85px]" priority />
           </div>
-          <div className='flex flex-col items-start'>
+          <div className='flex items-start flex-col w-50'>
             <span>{title}</span>
             <span>{name}</span>
             <span>{phone}</span>
           </div>
+          <div className="star-rating">
+      <div className="star-rating__wrap after:table after:clear-both" onClick={getStars} id={String(id)}>
+        <input className="star-rating__input" id="star-rating-5" type="radio" name="rating" value="5" />
+        <label className="star-rating__ico c fa fa-star-o fa-lg"  htmlFor="star-rating-5" title="5 out of 5 stars"></label>
+        <input className="star-rating__input" id="star-rating-4" type="radio" name="rating" value="4" />
+        <label className="star-rating__ico before:content-['\2605'] c fa fa-star-o fa-lg" htmlFor="star-rating-4" title="4 out of 5 stars"></label>
+        <input className="star-rating__input" id="star-rating-3" type="radio" name="rating" value="3" />
+        <label className="star-rating__ico c fa fa-star-o fa-lg" htmlFor="star-rating-3" title="3 out of 5 stars"></label>
+        <input className="star-rating__input" id="star-rating-2" type="radio" name="rating" value="2" />
+        <label className="star-rating__ico c fa fa-star-o fa-lg" htmlFor="star-rating-2" title="2 out of 5 stars"></label>
+        <input className="star-rating__input" id="star-rating-1" type="radio" name="rating" value="1" />
+        <label className="star-rating__ico c fa fa-star-o fa-lg" htmlFor="star-rating-1" title="1 out of 5 stars"></label>
+            </div>
+         </div>
           
         </li>
       })}
       </ul>
       </section>
     </main>
-   
   )
 }
 
@@ -63,8 +83,11 @@ export async function getServerSideProps() {
     }
   });
 
+  // const loc = await axios.get("https://api.mapbox.com/geocoding/v5/mapbox.places/50.4276926,30.4724321.json?access_token=pk.eyJ1IjoibWFya2trZWwiLCJhIjoiY2xhY2dtZXdxMDNsbDNwbnZ0ZXc5aGV6ZSJ9.nESzR0zRoJe6NJU4uW_IuA")
+  // console.log(loc.data);
   const data = await res.data;
-
+// 9.804124
+  // 147.139488
   return { props: {data} };
 
 }
